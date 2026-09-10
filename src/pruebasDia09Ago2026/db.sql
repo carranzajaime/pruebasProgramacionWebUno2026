baa6e068bd12ae704bfcdc79326e23e80a05425f
Dylan <carranzajaime6z9@gmail.com>
2026-09-09 20:00:07 -0600

    Pagina web más inicios de php

---
+++ src/pruebasDia09Ago2026/db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE crud_app;
+USE crud_app;
+
+CREATE TABLE usuarios{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    telefono VARCHAR(15) NOT NULL
+};
---
+++ src/pruebasDia09Ago2026/index.php
@@ -0,0 +1,21 @@
+<?<php>
+$nombre="Dylan";
+echo ($nombre);
+$edad= 20;
+echo ($edad);
+</php>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hola, mi nombre es $nombre y tengo $edad años.";
+        ?>
+</body>
+</html>
